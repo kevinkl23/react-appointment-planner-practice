3fd780b39a1120124d666a3d46ce0b6555fd49d4
kevinkl23 <lo.yohaneskevin@gmail.com>
2024-03-14 08:37:19 +0200

    Added Contacts Functionality

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
+import Root, { ROUTES } from "./components/root/Root";
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+import { useState } from "react";
+
+function App() {
+  /*
+  Define state variables for 
+  contacts and appointments 
+  */
+ const [contacts, setContacts] = useState([]);
+ const [appointments, setAppointments] = useState([]);
+
+  /*
+  Implement functions to add data to
+  contacts and appointments
+  */
+ const addContacts = (name, phone, email) => {
+  const data = {
+    name: name,
+    phone: phone,
+    email: email
+  }
+
+  setContacts(prevData=> [...prevData, data])
+ }
+
+ const addAppointments = (name, contact, date, time) => {
+  const data = {
+    name: name,
+    contact: contact, 
+    date: date,
+    time: time
+  }
+
+  setAppointments(prevData=> [...prevData, data]);
+ }
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={ <Root/> }>
+      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
+      <Route path='contacts' element={ <ContactsPage contacts={contacts} addData={addContacts} /> /* Add props to ContactsPage */ }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage contacts={contacts} appointments={appointments} addAppointments={addAppointments}/> /* Add props to AppointmentsPage */ }/>
+    </Route>
+  ));
+  
+  return (
+    <RouterProvider router={router}/>
+  );
+}
+
+export default App;
---
+++ src/containers/contactsPage/ContactsPage.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useRef } from "react";
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
+
+export const ContactsPage = ( {contacts, addContacts } ) => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const isInitialRender = useRef(true);
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  }
+
+  const handlePhoneChange = event => {
+    setPhone(event.target.value);
+  }
+
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
+  }
+
+  const clearForm = () => {
+    setName('');
+    setPhone('');
+    setEmail('');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+    const isDuplicate = contacts.filter(contact => {
+      return (contact.name === name && contact.phone === phone && contact.email === email);
+    })
+
+    if(isDuplicate.length > 0) {
+      alert('Contact is already added');
+    }
+    else {
+      addContacts(name, phone, email);
+    }
+  };
+
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
+
+  useEffect(() => {
+    if (isInitialRender.current) {
+      isInitialRender.current = false;
+      return;
+    }
+
+    const findName = contacts.find(contact => {
+      return contact.name === name;
+    })
+
+    if(findName) {
+      clearForm();
+    }
+    else{
+      alert('Fail to Add Contacts');
+    }
+  }, [contacts]);
+
+
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2> 
+        <ContactForm name={name} phone={phone} email={email} handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange} handleEmailChange={handleEmailChange} handleSubmit={handleSubmit}/>
+      </section>
+      <hr />
+      <section> 
+        <h2>Contacts</h2>
+        <TileList contacts={contacts} />
+      </section>
+    </div>
+  );
+};
